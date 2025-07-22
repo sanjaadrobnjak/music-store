6664c673852e415459fe012450cabc5f288d0d53
sanjaadrobnjak <123840521+sanjaadrobnjak@users.noreply.github.com>
2025-07-22 18:12:57 +0200

    Delete backend/src/routes directory

--- backend/src/routes/product.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var product_controller_1 = require("../controllers/product.controller");
-var productRouter = express.Router();
-productRouter
-    .route("/getAllProducts")
-    .get(function (req, res) { return new product_controller_1.ProductController().getAllProducts(req, res); });
-exports.default = productRouter;
